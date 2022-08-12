9aecf178b20621710964535157a73b9d7c50ccb4
ahmedashar <ahmedashar12345@gmail.com>
2022-08-12 23:56:48 +0500

    add features

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var currentUpdateItem;
 function addItems() {
 
     
@@ -11,26 +12,27 @@ function addItems() {
     else{
     // create li
     var li = document.createElement('li');
+    li.setAttribute('class','list_items')
     li.innerHTML = input.value;
 
     // create delete button
     var btn1 = document.createElement('button');
     btn1.innerHTML = 'Delete';
     btn1.setAttribute('onclick', 'deleteItem(event)')
-
+    
     // create update button
     var btn2 = document.createElement('button');
-    btn2.innerHTML = 'Update';
+    btn2.innerHTML = 'Edit';
     btn2.setAttribute('onclick', 'updateItem(event)')
-
+    
     
     ul.appendChild(li);
-
+    
     li.appendChild(btn1);
     li.appendChild(btn2);
-
     
- // empty input field after adding an item.
+    
+    // empty input field after adding an item.
     input.value = '';
     }
 }
@@ -41,18 +43,61 @@ function deleteItem(event) {
     var ul = li.parentElement;
     ul.removeChild(li);
 }
+//// edit item by prompt
+
+// function updateItem(event){
+    //     // var btn2 = event.target;
+    //     // var li = btn2.parentElement;
+//     // var firstChildNode = li.firstChild.nodeValue;
+
+//     var firstChildNode = event.target.parentElement.firstChild.nodeValue;
+
+//     // console.log(firstChildNode)
+    
+//     var updatedValue = prompt('update the value' , firstChildNode);
+//     // li.firstChild.nodeValue  = updatedValue;
+//     event.target.parentElement.firstChild.nodeValue  = updatedValue;
+// }
 
 function updateItem(event){
-    // var btn2 = event.target;
-    // var li = btn2.parentElement;
-    // var firstChildNode = li.firstChild.nodeValue;
+    var input = document.getElementById('take_input');
 
+    // get the text of li 
     var firstChildNode = event.target.parentElement.firstChild.nodeValue;
 
-    // console.log(firstChildNode)
+    // currentUpdateItem is a globle viriable, assign li to it.
+    currentUpdateItem = event.target.parentElement;
     
-    var updatedValue = prompt('update the value' , firstChildNode);
-    // li.firstChild.nodeValue  = updatedValue;
-    event.target.parentElement.firstChild.nodeValue  = updatedValue;
+    input.value = firstChildNode;
+    
+    // get buttons of inputField (for toggle purpose)
+    var inputFieldBtn = document.getElementsByClassName('input_button');
+    var addBtn = inputFieldBtn[0];
+    var editBtn = inputFieldBtn[1];
+    
+    addBtn.style.display = 'none'
+    editBtn.style.display = 'inline'
+
+    
 }
 
+
+
+function addEditedItem(){
+
+       var input = document.getElementById('take_input');
+       
+       currentUpdateItem.firstChild.nodeValue = input.value
+
+       // get buttons of inputField
+       var inputFieldBtn = document.getElementsByClassName('input_button');
+       var addBtn = inputFieldBtn[0];
+       var editBtn = inputFieldBtn[1];
+   
+       editBtn.style.display = 'none'
+       addBtn.style.display = 'inline'
+       
+       input.value = ''
+      
+
+}
